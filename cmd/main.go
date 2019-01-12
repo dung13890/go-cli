7f865b6632499a0cb3c240c22ee6b2779ad5ea74
Mr. D <dung13890@gmail.com>
2019-01-12 22:08:25 +0700

    truck tool

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "log"
-    "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 
-    cli "github.com/dung13890/go-cli"
+	cli "github.com/dung13890/go-cli"
 )
 
 func main() {
-    cmd := cli.New("truck")
-    cmd.Version = "1.0.0"
-    cmd.Usage = "[command] [-option]"
-    cmd.Run()
-    //getName()
-    // switchFlags()
-    // showHelp()
+	cmd := cli.New("truck")
+	cmd.Version = "1.0.0"
+	cmd.Usage = "[command] [-option]"
+	cmd.Run()
+	//getName()
+	// switchFlags()
+	// showHelp()
 }
 
 func showHelp() {
-    fmt.Print(`
+	fmt.Print(`
 USAGE: CLI Template [OPTIONS]
 
 OPTIONS:
@@ -30,37 +30,37 @@ OPTIONS:
 
 Commands:
  app            App arguments
-    `)
+	`)
 }
 
 func showVersion() {
-    fmt.Println("version")
+	fmt.Println("version")
 }
 
 func getName() {
-    reader := bufio.NewReader(os.Stdin)
-    fmt.Print("What is your name? ")
-    text, _ := reader.ReadString('\n')
-    fmt.Printf("Hello: %s, May I help you?\n", text)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("What is your name? ")
+	text, _ := reader.ReadString('\n')
+	fmt.Printf("Hello: %s, May I help you?\n", text)
 }
 
 func switchFlags() {
-    wordPtr := flag.String("word", "foo", "a string")
-    flag.Parse()
+	wordPtr := flag.String("word", "foo", "a string")
+	flag.Parse()
 
-    if len(flag.Args()) < 1 {
-        return
-    }
+	if len(flag.Args()) < 1 {
+		return
+	}
 
-    subArgs := flag.Args()[1:]
-    switch flag.Args()[0] {
-    case "version":
-        showVersion()
-    case "help", "usage":
-        showHelp()
-    default:
-        log.Fatalf("\nInvalid Command %s", os.Args[1])
-    }
-    fmt.Println(subArgs)
-    fmt.Println("word:", *wordPtr)
+	subArgs := flag.Args()[1:]
+	switch flag.Args()[0] {
+	case "version":
+		showVersion()
+	case "help", "usage":
+		showHelp()
+	default:
+		log.Fatalf("\nInvalid Command %s", os.Args[1])
+	}
+	fmt.Println(subArgs)
+	fmt.Println("word:", *wordPtr)
 }
